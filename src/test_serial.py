8087b2d342e9953aea4245ea6990ebfb3a2bc8b0
pmp-p <mail.peny@free.fr>
2023-01-20 23:19:47 +0100

    add CH34x

--- src/test_serial.py
+++ src/test_serial.py
@@ -3,7 +3,7 @@
 """
 tested on wemos D1 mini esp8266
 pip3 install esptool --user
-esptool.py --chip esp8266- -port /dev/ttyUSB? erase_flash
+esptool.py --chip esp8266 --port /dev/ttyUSB? erase_flash
 esptool.py --chip esp8266 --port /dev/ttyUSB? --baud 1000000 write_flash --flash_size=4MB -fm dio 0 esp8266-1m-20230120-unstable-v1.19.1-831-g4f3780a15.bin
 
 """
