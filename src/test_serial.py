a7adf5cfe51895b78a3f199944d3504427a242ca
pmp-p <mail.peny@free.fr>
2023-01-20 22:25:35 +0100

    update

--- src/test_serial.py
+++ src/test_serial.py
@@ -1,3 +1,14 @@
 import asyncio
 import pygame
+import pygame_widgets
+
+
+async def main():
+    print("\n"*20)
+    await input("Press Enter to connect usb serial port\n\n")
+    await jsiter( window.io.open_serial())
+    print("connected!")
+
+
+asyncio.run(main())
 
