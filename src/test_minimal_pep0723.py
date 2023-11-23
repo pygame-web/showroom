008c3b32a54bbfc80c743b50b99fd51afc76ff3c
pmp-p <mail.peny@free.fr>
2023-11-23 15:10:31 +0100

    add minimal

---
+++ src/test_minimal_pep0723.py
@@ -0,0 +1,46 @@
+import asyncio
+
+# /// pyproject
+# [project]
+# name = "test_pygame"
+# version = "version"
+# description = "description"
+# readme = {file = "README.txt", content-type = "text/markdown"}
+# requires-python = ">=3.11"
+#
+# dependencies = [
+#    "pygame.base",
+# ]
+# ///
+
+
+
+
+import pygame
+
+pygame.init()
+
+# set up the window
+screen_width, screen_height = 640, 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("blank")
+
+# main game loop
+async def main():
+
+    while True:
+        # handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        # clear the screen
+        screen.fill((0, 128, 0))
+
+        # update the screen
+        pygame.display.update()
+        await asyncio.sleep(0)
+        pygame.time.Clock().tick(120)
+
+asyncio.run(main())
