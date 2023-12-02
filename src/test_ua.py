a4b43610a83a81245773b805b7b0182602528611
pmp-p <mail.peny@free.fr>
2023-12-02 03:11:22 +0100

    add ua test

---
+++ src/test_ua.py
@@ -0,0 +1,29 @@
+import pygbag.aio as asyncio
+import platform
+
+# /// pyproject
+# [project]
+# name = "test_pygame"
+# version = "version"
+# description = "description"
+# readme = {file = "README.txt", content-type = "text/markdown"}
+# requires-python = ">=3.11"
+#
+# dependencies = [
+#    "pypng",
+#    "qrcode",
+# ]
+# ///
+
+
+if 1:
+    print("\n"*10)
+    print("="*80)
+    print()
+    ua = platform.window.navigator.userAgent
+    print( ua )
+    print()
+    print("="*80)
+    from qrcode_term import qrcode_string
+    print(qrcode_string(ua))
+
