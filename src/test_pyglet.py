708d109c44873bd5aef6ecbedb4046091986aa83
pmp-p <mail.peny@free.fr>
2023-12-12 14:21:19 +0100

    add pyglet test

---
+++ src/test_pyglet.py
@@ -0,0 +1,135 @@
+import asyncio
+import os
+
+# /// pyproject
+# [project]
+# name = "name"
+# version = "version"
+# description = "description"
+# readme = {file = "README.txt", content-type = "text/markdown"}
+# requires-python = ">=3.11"
+#
+# dependencies = [
+#     "Pillow",
+#     "pyglet",
+# ]
+# ///
+
+
+import pyglet
+import math
+
+from pyglet import shapes
+
+if 1:
+
+    # Shapes
+    class Demo(pyglet.window.Window):
+        WIDTH = 720
+        HEIGHT = 480
+
+        def __init__(self, width, height):
+            super().__init__(width, height, "Shapes")
+            self.time = 0
+            self.batch = pyglet.graphics.Batch()
+
+            self.circle = shapes.Circle(360, 240, 75, color=(255, 225, 255, 127), batch=self.batch)
+
+            # Rectangle with center as anchor
+            self.square = shapes.BorderedRectangle(360, 240, 100, 100, border=5, color=(55, 55, 255),
+                                                   border_color=(25, 25, 25), batch=self.batch)
+            self.square.anchor_position = 50, 50
+
+            # Large transparent rectangle
+            self.rectangle = shapes.Rectangle(100, 190, 500, 100, color=(255, 22, 20, 64), batch=self.batch)
+
+            self.line = shapes.Line(0, 0, 0, 480, width=4, color=(200, 20, 20), batch=self.batch)
+
+            self.triangle = shapes.Triangle(10, 10, 190, 10, 100, 150, color=(55, 255, 255, 175), batch=self.batch)
+
+            septagon_step = math.pi * 2 / 7
+            self.fading_septagon = shapes.Polygon(
+                *[[50 + 40 * math.sin(i * septagon_step), 200 + 40 * math.cos(i * septagon_step)] for i in range(7)],
+                batch=self.batch
+            )
+
+            self.arc = shapes.Arc(50, 300, radius=40, segments=25, angle=4, color=(255, 255, 255), batch=self.batch)
+
+            self.star = shapes.Star(600, 375, 50, 30, 5, color=(255, 255, 0), batch=self.batch)
+
+            self.ellipse = shapes.Ellipse(650, 150, a=50, b=30, color=(55, 255, 55), batch=self.batch)
+
+            self.sector = shapes.Sector(125, 400, 60, angle=0.9, color=(55, 255, 55), batch=self.batch)
+
+            self.polygon = shapes.Polygon([400, 100], [500, 10], [600, 100], [550, 175], [450, 150], batch=self.batch)
+
+            self.box = shapes.Box(60, 40, 200, 100, thickness=2, color=(244, 55, 55), batch=self.batch)
+
+        def on_draw(self):
+            """Clear the screen and draw shapes"""
+            self.clear()
+            self.batch.draw()
+
+        def update(self, delta_time):
+            """Animate the shapes"""
+            self.time += delta_time
+            self.square.rotation = self.time * 15
+            self.rectangle.y = 200 + math.sin(self.time) * 190
+            self.circle.radius = 75 + math.sin(self.time * 1.17) * 25
+            self.triangle.rotation = self.time * 15
+
+            self.line.x = 360 + math.sin(self.time * 0.81) * 360
+            self.line.x2 = 360 + math.sin(self.time * 1.34) * 360
+
+            self.arc.rotation = self.time * 30
+
+            self.fading_septagon.opacity = int(255 * (0.5 + (0.5 * math.cos(self.time))))
+
+            self.star.rotation = self.time * 50
+            self.polygon.rotation = self.time * 45
+
+            self.ellipse.b = abs(math.sin(self.time) * 100)
+            self.sector.angle = self.time % math.tau
+
+
+elif 1:
+    # Hello world
+    class Demo(pyglet.window.Window):
+        WIDTH = 1024
+        HEIGHT = 600
+        def __init__(self, width, height):
+            super().__init__(width, height, "Hello")
+
+            self.label = pyglet.text.Label('Hello, world!',
+                                      font_size=36,
+                                      x=self.width // 2,
+                                      y=self.height // 2,
+                                      anchor_x='center',
+                                      anchor_y='center')
+
+        def update(self, dt):
+            self.clear()
+            self.label.draw()
+
+if 0:
+    async def main():
+
+        demo = Demo(1024, 600)
+        pyglet.clock.schedule_interval(demo.update, 1/30)
+
+        self = pyglet.app.event_loop
+        self.has_exit = False
+        from pyglet.window import Window
+        Window._enable_event_queue = False
+
+        while not self.has_exit:
+            timeout = self.idle()
+            self._redraw_windows(1/60)
+            pyglet.app.platform_event_loop.step(timeout)
+            await asyncio.sleep(0)
+
+    asyncio.run(main())
+else:
+    demo = Demo(Demo.WIDTH, Demo.HEIGHT)
+    pyglet.clock.schedule_interval(demo.update, 1/30)
+    pyglet.app.run()
