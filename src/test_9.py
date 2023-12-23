75e399648c82a182318e62ded0e26bbde9d5c1f1
pmp-p <mail.peny@free.fr>
2023-12-23 21:56:40 +0100

    fix py ver

--- src/test_9.py
+++ src/test_9.py
@@ -1,4 +1,18 @@
 
+# /// pyproject
+# [project]
+# name = "name"
+# version = "version"
+# description = "description"
+# readme = {file = "README.txt", content-type = "text/markdown"}
+# requires-python = ">=3.11"
+#
+# dependencies = [
+#     "pygame-ce",
+# ]
+# ///
+
+
 import pygbag.aio as asyncio
 import aio.fetch
 
