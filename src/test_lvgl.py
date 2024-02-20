a2d75bbdf9cedfa18b6110d9ea338d7869adbc00
pmp-p <mail.peny@free.fr>
2024-02-20 09:18:44 +0100

    fix lvgl import

--- src/test_lvgl.py
+++ src/test_lvgl.py
@@ -1,14 +1,8 @@
 import pygbag.aio as asyncio
 
-# /// pyproject
-# [project]
-# name = "name"
-# version = "version"
-# description = "description"
-# readme = {file = "README.txt", content-type = "text/markdown"}
-# requires-python = ">=3.11"
-#
+# /// script
 # dependencies = [
+#     "cffi",
 #     "lvgl",
 # ]
 # ///
