86a2bde7b51b1aa235a2d0a1698c8315a8c19698
pmp-p <mail.peny@free.fr>
2024-03-20 14:37:51 +0100

    numpy load test

---
+++ src/test_numpy.py
@@ -0,0 +1,28 @@
+# /// script
+# dependencies = [
+#    "numpy",
+# ]
+# ///
+
+
+async def main():
+    import numpy
+    print(f"""
+
+
+
+
+
+
+    {numpy=}
+    
+    {numpy.version.full_version}
+
+
+
+
+""")
+
+
+asyncio.run(main())
+
